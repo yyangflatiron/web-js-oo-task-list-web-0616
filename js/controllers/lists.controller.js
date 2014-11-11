deaa73e234505387829d4ce5854fa9d79be88543
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-11 11:27:57 -0500

    converts to new prototype

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,33 +1,40 @@
 // Lists Controller
 function ListsController() {
-  this.list = new List();
+  this.$addListForm = $('#add_list'),
+  this.$listTitleInput = $('#list_title'),
+  this.$selectListMenu = $('#select_list'),
+  this.$addTaskForm = $('#add_task'),
+  this.$wrapper = $('#wrapper');
 }
-ListsController.prototype = {
-  constructor: ListsController,
-  init: function() {
-    
-    var $addListForm = $('#add_list'),
-        $listTitleInput = $('#list_title'),
-        $selectListMenu = $('#select_list'),
-        $addTaskForm = $('#add_task'),
-        $wrapper = $('#wrapper');
 
-    $addTaskForm.hide(); // initially hide add task form
-
-    $addListForm.submit(function(event) {
-      event.preventDefault();
-      var listTitle = $listTitleInput.val(),
-          list = new List(listTitle);
-      list.build();
-      $addTaskForm.one().show();
-      $listTitleInput.val('');
-    });
-
-    $wrapper.on('click', '.destroy-list', function(){ //live event listener
-      var listId = parseInt($(this).parents('h2').next('ul').data('id'));
-      List.all.splice(listId, 1, null);
-      $selectListMenu.find('option[value="'+listId+'"]').remove();
-      $(this).parents('.list').remove();
-    });
-  }
+ListsController.prototype.hideTaskForm = function(){
+  this.$addTaskForm.hide(); // initially hide add task form
 };
+
+ListsController.prototype.addListFormListener = function() {
+  var self = this;
+  this.$addListForm.submit(function(event) {
+    event.preventDefault();
+    var listTitle = self.$listTitleInput.val(),
+        list = new List(listTitle);
+    list.build();
+    self.$addTaskForm.one().show();
+    self.$listTitleInput.val('');
+  });
+};
+
+ListsController.prototype.destroyListLiveEventListener = function(){
+  var self = this;
+  this.$wrapper.on('click', '.destroy-list', function(){ //live event listener
+    var listId = parseInt($(this).parents('h2').next('ul').data('id'));
+    List.all.splice(listId, 1, null);
+    self.$selectListMenu.find('option[value="'+listId+'"]').remove();
+    $(this).parents('.list').remove();
+  });
+};
+
+ListsController.prototype.init = function() {
+  this.hideTaskForm();
+  this.addListFormListener();
+  this.destroyListLiveEventListener();
+}
