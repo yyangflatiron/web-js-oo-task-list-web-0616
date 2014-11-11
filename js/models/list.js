cc1835e2e54171bffb78b4991002e65b595f545e
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-11 11:44:39 -0500

    adds back deleted solution for models

--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,23 @@
 'use strict';
 // List Model
 
+function List(title) {
+  this.id = this.constructor.all.length;
+  this.constructor.all.push(this);
+  this.title = title;
+  this.tasks = [];
+}
+List.prototype = {
+  constructor: List,
+  listEle: function() {
+    return '<div class="list"><h2><button class="destroy-list">x</button> '+this.title+'</h2><ul id="list-'+this.id+'" data-id="'+this.id+'"></ul></div>';
+  },
+  optionEle: function() {
+    return '<option value="'+this.id+'">'+this.title+'</option>';
+  },
+  build: function() {
+     $('#lists').append(this.listEle());
+     $('#select_list').append(this.optionEle());
+  }
+};
+List.all = [];
