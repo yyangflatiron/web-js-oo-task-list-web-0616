644751f67bbec1212b3093e6e93b8ff34d3ee3a4
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-11 11:57:39 -0500

    adds spec helper and fixes code running between tests

--- spec/list.spec.js
+++ spec/list.spec.js
@@ -1,15 +1,10 @@
 'use strict';
-describe('List Model', function() {
+(function(){ describe('List Model', function() {
 
   describe('Constructor Properties', function() {
 
-
-    describe('all', function() {
-
-      it('should have an "all" property to hold instances of each list created', function() {
-        expect(List.all).toEqual([]);
-      });
-
+    it('should have an "all" property to hold instances of each list created', function() {
+      expect(List.all).toEqual([]);
     });
 
   }); //end Constructor Properties
@@ -19,30 +14,27 @@ describe('List Model', function() {
 
     var list;
     beforeEach(function() {
+      emptyArr(List.all);
       list = new List('Jon\'s List');
     });
 
-    describe('title', function() {
-
-      it('should be have a title when a new List is created', function() {
-        expect(list.title).toBe('Jon\'s List');
-      });
+    it('should be have a title when a new List is created', function() {
+      expect(list.title).toBe('Jon\'s List');
     });
 
-    describe('id', function() {
 
-      it('should be assigned an autoincrementing id when a new List is created', function() {
-        expect(list.id).toBe(1);
-      });    
-    });
+    it('should be assigned an autoincrementing id when a new List is created', function() {
+      var secondList = new List('Tristan\'s List');
+      expect(list.id).toBe(0);
+      expect(secondList.id).toBe(1);
+    });    
 
-    describe('tasks', function() {
 
-      it('should be assigned an empty array when a new List is created', function() {
-        expect(list.tasks).toEqual([]);
-      });      
-    });
+    it('should be assigned an empty array when a new List is created', function() {
+      expect(list.tasks).toEqual([]);
+    });      
 
   }); //Instance Properties
 
 }); //end List Model tests
+})();
