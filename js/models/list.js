08e88bdf570b3362c043c1a44a925a8cc7d6372a
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-12 11:57:46 -0500

    corrects prototypical style for list

--- js/models/list.js
+++ js/models/list.js
@@ -7,17 +7,18 @@ function List(title) {
   this.title = title;
   this.tasks = [];
 }
-List.prototype = {
-  constructor: List,
-  listEl: function() {
-    return '<div class="list"><h2><button class="destroy-list">x</button> '+this.title+'</h2><ul id="list-'+this.id+'" data-id="'+this.id+'"></ul></div>';
-  },
-  optionEl: function() {
-    return '<option value="'+this.id+'">'+this.title+'</option>';
-  },
-  build: function() {
-     $('#lists').append(this.listEl());
-     $('#select_list').append(this.optionEl());
-  }
+
+List.prototype.listEl = function() {
+  return '<div class="list"><h2><button class="destroy-list">x</button> '+this.title+'</h2><ul id="list-'+this.id+'" data-id="'+this.id+'"></ul></div>';
 };
+
+List.prototype.optionEl = function() {
+  return '<option value="'+this.id+'">'+this.title+'</option>';
+};
+
+List.prototype.build = function() {
+   $('#lists').append(this.listEl());
+   $('#select_list').append(this.optionEl());
+};
+
 List.all = [];
