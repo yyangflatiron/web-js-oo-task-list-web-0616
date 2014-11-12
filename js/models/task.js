9c80408e92784d12226fd543be55509a5aba6f45
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-12 11:16:59 -0500

    fixes the task model up

--- js/models/task.js
+++ js/models/task.js
@@ -1,19 +1,21 @@
 'use strict';
 // Task Model
-
 function Task(description, priority, list) {
   this.description = description;
   this.priority = priority;
   this.list = list;
   this.id = this.list.tasks.length;
   this.list.tasks.push(this);
+  this.constructor.all.push(this);
 }
-Task.prototype = {
-  constructor: Task,
-  el: function() {
-    return '<li data-id="'+this.id+'"><button class="destroy-task">x</button> '+this.description+', '+this.priority+'</li>';
-  },
-  build: function() {
-    $('#list-'+this.list.id).append(this.el());
-  }
+
+Task.all = [];
+
+Task.prototype.el = function() {
+  return '<li data-id="'+this.id+'"><button class="destroy-task">x</button> '+this.description+', '+this.priority+'</li>';
 };
+
+Task.prototype.build = function() {
+  $('#list-'+this.list.id).append(this.el());
+};
+
